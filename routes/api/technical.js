024bbbb828d611934a31e8468060c88044685160
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2025-01-22 14:55:40 +0200

    Update technical.js

--- routes/api/technical.js
+++ routes/api/technical.js
@@ -2,13 +2,6 @@ const express = require("express");
 const { ctrlWrapper } = require("../../helpers");
 const ctrl = require("../../controllers/technicalController");
 
-const {
-  validateBody,
-  isValidId,
-  authorize,
-  authenticateRefresh,
-} = require("../../middlewares");
-
 const router = express.Router();
 
 // get technical data
